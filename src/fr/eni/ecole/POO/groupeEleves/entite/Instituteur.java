1aeb0498db7e4b559c4349cedda9679a29bca2c1
unknown <geoffreygirard28@gmail.com>
2023-02-18 08:52:43 +0100

    add Personne, Parent & Instituteur Class

---
+++ src/fr/eni/ecole/POO/groupeEleves/entite/Instituteur.java
@@ -0,0 +1,17 @@
+package fr.eni.ecole.POO.groupeEleves.entite;
+
+import java.time.LocalDate;
+
+public class Instituteur extends Personne {
+
+	public Instituteur() {
+		super();
+	}
+	
+	public Instituteur(String nom, String prenom, String adresse, LocalDate ddn) {
+		super(nom, prenom, adresse, ddn);
+	}
+	
+	
+
+}
--- src/fr/eni/ecole/POO/groupeEleves/entite/Personne.java
+++ src/fr/eni/ecole/POO/groupeEleves/entite/Personne.java
@@ -1,5 +1,97 @@
 package fr.eni.ecole.POO.groupeEleves.entite;
 
-public class Personne {
+import java.time.LocalDate;
 
+public class Personne {
+	
+	public String nom;
+	public String prenom;
+	public String adresse;
+	public LocalDate ddn;
+	
+	
+	/**
+	 * 
+	 */
+	public Personne() {
+		// Constructeur simple
+	}
+	
+	/**
+	 * 
+	 * @param nom
+	 * @param prenom
+	 * @param adresse
+	 * @param ddn
+	 */
+	public Personne(String nom, String prenom, String adresse, LocalDate ddn) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+		this.ddn = ddn;
+	}
+	
+	
+	
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	/**
+	 * @return the adresse
+	 */
+	public String getAdresse() {
+		return adresse;
+	}
+
+	/**
+	 * @param adresse the adresse to set
+	 */
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	/**
+	 * @return the ddn
+	 */
+	public LocalDate getDdn() {
+		return ddn;
+	}
+
+	/**
+	 * @param ddn the ddn to set
+	 */
+	public void setDdn(LocalDate ddn) {
+		this.ddn = ddn;
+	}
+
+	@Override
+	public String toString() {
+		return "Personne[nom= " + nom + ", prénom= " + prenom + ", adresse= " + adresse + "Date de naissance=" + ddn + "]";
+	}
+	
 }
