bbc4b2765be8ab70bbf1fe4e7da23dcd673c3a8d
unknown <geoffreygirard28@gmail.com>
2023-02-18 09:52:14 +0100

    add ClasseException

--- src/fr/eni/ecole/POO/groupeEleves/entite/Classe.java
+++ src/fr/eni/ecole/POO/groupeEleves/entite/Classe.java
@@ -2,17 +2,19 @@ package fr.eni.ecole.POO.groupeEleves.entite;
 
 public class Classe {
 
-	public String nom;
-	public int nbEleves;
-	public Eleve[] lstEleves;
-	public Parent[] lstParent;
-	public Instituteur instituteur;
+	private String nom;
+	private int nbEleves;
+	private Eleve[] lstEleves;
+	private Parent[] lstParent;
+	private Instituteur instituteur;
+	private final int NB_MAX = 20;
 	
 	/**
 	 * 
 	 */
 	public Classe() {
-		
+		nbEleves = 0;
+		lstEleves = new Eleve[NB_MAX];
 	}
 	
 	/**
@@ -24,16 +26,27 @@ public class Classe {
 	
 	/** 
 	 * @param lstEleve
+	 * @throws ClasseException
 	 */
-	public void addEleve(Eleve e) {
-		lstEleves[nbEleves++] = e;
+	public void addEleve(Eleve e) throws ClasseException {
+		if (nbEleves < NB_MAX) {
+			lstEleves[nbEleves++] = e;			
+		} else {
+			throw new ClasseException("Nombre max d'eleves atteint pour cette classe.");
+		}
 	}
 
 	/**
 	 * @return the eleve
 	 */
-	public Eleve getEleve(int index) {
-		return lstEleves[index];
+	public Eleve getEleve(int index) throws ClasseException {
+		if (index >= 0 && index < nbEleves) {
+			// Alors l'index existe dans le tableau et il y a un éléve renseigné
+			return lstEleves[index];
+		} else {
+			// L'index est incorrect.
+			throw new ClasseException("La classe ne contient que " + nbEleves + " élèves");
+		}
 	}
 	
 	/**
@@ -46,7 +59,7 @@ public class Classe {
 	/**
 	 * @return the lstParent
 	 */
-	public Parent[] getLstParent() {
+	public Parent[] getListParent() {
 		return lstParent;
 	}
 	
@@ -58,9 +71,31 @@ public class Classe {
 	}
 	
 	/**
+	 * @param instituteur
+	 */
+	public void setInstituteur(Instituteur instituteur) {
+		this.instituteur = instituteur;
+	}
+	
+	/**
 	 * @return the instituteur
 	 */
 	public Instituteur getInstituteur() {
 		return instituteur;
+	}
+	
+	public String toString() {
+		StringBuffer result = new StringBuffer();
+		result.append("---------------------------------------------------").append("\n");
+		result.append("Classe : ").append(this.nom).append("\n");
+		result.append("   Instituteur : ").append(this.instituteur.getPrenom()).append(" ")
+				.append(this.instituteur.getNom()).append("\n");
+		result.append("     avec :").append("\n");
+		for (int i = 0; i < nbEleves; i++) {
+			Eleve eleve = lstEleves[i];
+			result.append("     ").append(eleve.getPrenom()).append(" ").append(eleve.getNom()).append("\n");
+		}
+		result.append("---------------------------------------------------");
+		return result.toString();
 	}
 }
--- src/fr/eni/ecole/POO/groupeEleves/entite/ClasseException.java
+++ src/fr/eni/ecole/POO/groupeEleves/entite/ClasseException.java
@@ -1,6 +1,10 @@
 package fr.eni.ecole.POO.groupeEleves.entite;
 
-public class ClasseException {
+public class ClasseException extends Exception {
 
+	private static final long serialVersionUID = 1L;
 	
+	public ClasseException(String message) {
+		super(message);
+	}
 }
